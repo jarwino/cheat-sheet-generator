ad345d04dda365a1b97a769eaeb285be34b98ea6
jarwino <jarwino@gmail.com>
2017-12-13 20:36:39 -0500

    subprocess error

--- cheat.py
+++ cheat.py
@@ -1,6 +1,49 @@
 import sys, math
-from pdf2image import convert_from_path
+from subprocess import Popen, PIPE
 from PIL import Image
+
+def __parse_pdftoppm_buffer(data):
+    images = []
+
+    index = 0
+
+    while(index < len(data)):
+        code, size, rgb = tuple(data[index:index + 40].split(b'\n')[0:3])
+        size_x, size_y = tuple(size.split(b' '))
+        file_size = len(code) + len(size) + len(rgb) + 3 + int(size_x) * int(size_y) * 3
+        images.append(Image.open(BytesIO(data[index:index + file_size])))
+        index += file_size
+
+    return images
+
+def __load_from_output_folder(output_folder):
+    return [Image.open(os.path.join(output_folder, f)) for f in sorted(os.listdir(output_folder))]
+
+
+def convert_from_path(pdf_path, dpi=200, output_folder=None):
+    """
+        Description: Convert PDF to Image will throw whenever one of the condition is reached
+        Parameters:
+            pdf_path -> Path to the PDF that you want to convert
+            dpi -> Image quality in DPI (default 200)
+            output_folder -> Write the resulting images to a folder (instead of directly in memory)
+    """
+
+    args = ['pdftoppm', '-r', str(dpi), pdf_path, ]
+
+    if output_folder is not None:
+        args.append(output_folder if output_folder[-1] == '/' else output_folder + '/')
+        args.append('shell=True')
+
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+
+    data, err = proc.communicate()
+
+    if output_folder is not None:
+        return __load_from_output_folder(output_folder)
+    else:
+        return __parse_pdftoppm_buffer(data)
+
 
 images = convert_from_path(sys.argv[1])
 widths, heights = zip(*(i.size for i in images))
@@ -36,3 +79,4 @@ for i in range(num_pages):
         im += 1
 
     new_im.save('page' + str(i + 1) + '.png')
+
